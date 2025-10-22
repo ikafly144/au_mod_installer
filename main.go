471c84e87c1914ddd8f8f963b47d877200404f37
ikafly144 <support@sabafly.net>
2025-10-22 09:53:05 +0900

    Remove unused variables and initialization logic for game path detection

--- main.go
+++ main.go
@@ -1,37 +1,13 @@
 package main
 
 import (
-	"au_mod_installer/pkg/aumgr"
 	"au_mod_installer/ui"
 	"os"
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/lang"
-	"fyne.io/fyne/v2/widget"
 	"github.com/sqweek/dialog"
 )
-
-var (
-	detectedGamePath string             = "" // Example: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Among Us\\"
-	detectedLauncher aumgr.LauncherType = aumgr.LauncherUnknown
-	launchButton     *widget.Button
-
-	selectedModIndex int = 0
-
-	initErr error = nil
-)
-
-func init() {
-	gamePath, err := aumgr.GetAmongUsDir()
-	if err == nil {
-		detectedGamePath = gamePath
-	}
-	detectedLauncher = aumgr.DetectLauncherType(detectedGamePath)
-	if detectedLauncher == aumgr.LauncherUnknown {
-		detectedGamePath = ""
-		return
-	}
-}
 
 func main() {
 	a := app.New()
