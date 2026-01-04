2edd8f1268a4725bb60121f1ea592177001cc635
ikafly144 <ikafly144@sabafly.net>
2026-01-05 00:10:15 +0900

    fix: "app.already_running"メッセージを更新し、他のインスタンスが強制終了されたことを通知

--- client/main.go
+++ client/main.go
@@ -9,6 +9,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+	"syscall"
+	"unsafe"
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/lang"
@@ -18,12 +21,15 @@ import (
 	"github.com/ikafly144/au_mod_installer/common/versioning"
 	"github.com/nightlyone/lockfile"
 	"github.com/sqweek/dialog"
+	"github.com/zzl/go-win32api/win32"
+	"golang.org/x/sys/windows"
 )
 
 var DefaultServer = "https://modofus.sabafly.net/v1"
 
 func main() {
-	lock, err := lockfile.New(filepath.Join(os.Getenv("PROGRAMDATA"), "au_mod_installer.lock"))
+	lockPath := filepath.Join(os.Getenv("PROGRAMDATA"), "au_mod_installer.lock")
+	lock, err := lockfile.New(lockPath)
 	if err != nil {
 		slog.Error("Failed to create lockfile", "error", err)
 		os.Exit(1)
@@ -31,8 +37,55 @@ func main() {
 	err = lock.TryLock()
 	if err != nil {
 		slog.Error("Another instance is already running", "error", err)
-		//ignore:printf
-		(&dialog.MsgBuilder{Msg: lang.LocalizeKey("app.already_running", "Another instance of Among Us Mod Installer is already running.")}).Title(lang.LocalizeKey("app.error", "Error")).Error()
+		owner, err := lock.GetOwner()
+		if current, err1 := os.FindProcess(os.Getpid()); err == nil && err1 == nil {
+			slog.Info("Lockfile owned by", "pid", owner.Pid)
+			slog.Info("Current process pid", "pid", current.Pid)
+			if owner.Pid == current.Pid {
+				slog.Info("Lockfile owned by current process, unlocking")
+				_ = os.Remove(lockPath)
+			} else {
+				found := false
+				if err := windows.EnumWindows(syscall.NewCallback(func(hwnd windows.HWND, lparam uintptr) int {
+					var pid uint32
+					tid, err := windows.GetWindowThreadProcessId(hwnd, &pid)
+					if err != nil || tid == 0 {
+						return 1
+					}
+					if int(pid) == owner.Pid {
+						var classNamePtr [256]uint16
+						if _, err := windows.GetClassName(hwnd, &classNamePtr[0], int32(len(classNamePtr))); err != nil {
+							slog.Error("Failed to get window class name", "error", err)
+						}
+						className := syscall.UTF16ToString(classNamePtr[:])
+						if !strings.Contains(className, "GLFW") && !strings.Contains(className, "NVOpenGL") {
+							return 1
+						}
+						slog.Info("Found window of existing process, bringing to foreground", "hwnd", hwnd, "pid", pid, "class", className)
+						win32.FlashWindowEx(&win32.FLASHWINFO{
+							CbSize:    uint32(unsafe.Sizeof(win32.FLASHWINFO{})),
+							Hwnd:      win32.HWND(hwnd),
+							DwFlags:   win32.FLASHW_TRAY | win32.FLASHW_TIMERNOFG,
+							UCount:    5,
+							DwTimeout: 0,
+						})
+						found = win32.SetForegroundWindow(win32.HWND(hwnd)) != win32.FALSE
+						return 1
+					}
+					return 1
+				}), nil); err != nil {
+					slog.Error("Failed to enumerate windows", "error", err)
+				}
+				if found {
+					slog.Info("Brought existing instance to foreground, exiting")
+				} else {
+					if err := owner.Kill(); err != nil {
+						slog.Error("Failed to kill existing process", "error", err)
+					}
+					(&dialog.MsgBuilder{Msg: lang.LocalizeKey("app.error.already_running", "Another instance of Mod of Us was already running and has been forced to close. Please restart the application.")}).Title(lang.LocalizeKey("app.error", "Error")).Error()
+				}
+			}
+		}
 		_ = lock.Unlock()
 		os.Exit(1)
 	}
