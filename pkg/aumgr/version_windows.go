d159e4dc258a9597f08c6967faa784d1004615e2
ikafly144 <ikafly144@sabafly.net>
2026-03-30 00:01:16 +0900

    refactor(aumgr): DLLパスの取得とファイルの存在確認を整理

--- pkg/aumgr/version_windows.go
+++ pkg/aumgr/version_windows.go
@@ -3,7 +3,10 @@
 package aumgr
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"syscall"
@@ -22,15 +25,35 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	if _, err := os.Stat(filepath.Join(filepath.Dir(path), "lib", "dump-version.dll")); os.IsNotExist(err) {
-		if err := os.MkdirAll(filepath.Join(filepath.Dir(path), "lib"), os.ModePerm); err != nil {
+	dllPath := filepath.Join(filepath.Dir(path), "lib", "dump-version.dll")
+	if _, err := os.Stat(dllPath); os.IsNotExist(err) {
+		if err := os.MkdirAll(filepath.Join(filepath.Dir(dllPath), "lib"), os.ModePerm); err != nil {
 			panic(err)
 		}
-		if err := os.WriteFile(filepath.Join(filepath.Dir(path), "lib", "dump-version.dll"), dumpVersionDLL, 0644); err != nil {
+		if err := os.WriteFile(dllPath, dumpVersionDLL, 0644); err != nil {
 			panic(err)
+		}
+	} else if err != nil {
+		panic(err)
+	} else {
+		// Check hashsum to make sure the existing file is the same as the embedded one, if not, overwrite it
+		existingDLL, err := os.OpenFile(dllPath, os.O_RDONLY, 0444)
+		if err != nil {
+			panic(err)
+		}
+		defer existingDLL.Close()
+		hasher := sha256.New()
+		_, err = io.Copy(hasher, existingDLL)
+		if err != nil {
+			panic(err)
+		}
+		if !bytes.Equal(hasher.Sum(nil), new(sha256.Sum256(dumpVersionDLL))[:]) {
+			if err := os.WriteFile(dllPath, dumpVersionDLL, 0644); err != nil {
+				panic(err)
+			}
 		}
 	}
-	dll = syscall.NewLazyDLL(filepath.Join(filepath.Dir(path), "lib", "dump-version.dll"))
+	dll = syscall.NewLazyDLL(dllPath)
 }
 
 func readVersionFile(path string) (string, error) {
