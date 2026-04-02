aa854b7eb1681edd46a960bd32db7adb4bfa6ebf
ikafly144 <ikafly144@sabafly.net>
2026-04-02 14:47:24 +0900

    refactor(aumgr): LaunchAmongUs関数の引数を整理し、直接参加情報を統合

--- pkg/aumgr/launch_unsupported.go
+++ pkg/aumgr/launch_unsupported.go
@@ -4,6 +4,6 @@ package aumgr
 
 import "errors"
 
-func LaunchAmongUs(launcherType LauncherType, amongUsDir string, dllDir string, exchangeCode string, lobbyCode string, serverIP string, serverPort uint16, onStarted func(pid int) error) error {
+func LaunchAmongUs(launcherType LauncherType, amongUsDir string, dllDir string, exchangeCode string, directJoinInfo DirectJoinInfo, onStarted func(pid int) error) error {
 	return errors.New("launching Among Us is unsupported on this platform")
 }
