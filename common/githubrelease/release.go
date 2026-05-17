f65257a90bbc841ee5b2d940e099d7a798fba8b3
ikafly144 <ikafly144@sabafly.net>
2026-05-18 00:36:13 +0900

    chore(deps): github.com/google/go-githubをv86にアップデート

--- common/githubrelease/release.go
+++ common/githubrelease/release.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/google/go-github/v85/github"
+	"github.com/google/go-github/v86/github"
 )
 
 // GetLatestReleaseIncludingPrereleases returns the latest non-draft release
