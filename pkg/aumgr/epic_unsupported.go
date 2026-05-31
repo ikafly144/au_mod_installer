e836789eded8471e9c34d777beaafe69566f466b
ikafly144 <ikafly144@sabafly.net>
2026-05-31 22:38:47 +0900

    refactor(aumgr): EpicManifestおよび関連ファイルを削除し、未使用コードを整理

--- pkg/aumgr/epic_unsupported.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package aumgr
-
-import "errors"
-
-func getEpicManifest() (Manifest, error) {
-	return nil, errors.New("epic Games manifest detection is unsupported on this platform")
-}
